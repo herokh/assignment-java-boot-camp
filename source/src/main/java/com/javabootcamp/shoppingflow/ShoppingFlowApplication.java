8915e60aa9d68a4b7176f568f3ee89561ed5af19
Hero <phantom.mini@gmail.com>
2022-02-17 17:06:47 +0700

    build: initial java spring boot project #1

---
+++ source/src/main/java/com/javabootcamp/shoppingflow/ShoppingFlowApplication.java
@@ -0,0 +1,13 @@
+package com.javabootcamp.shoppingflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingFlowApplication.class, args);
+	}
+
+}
---
+++ source/src/test/java/com/javabootcamp/shoppingflow/ShoppingFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javabootcamp.shoppingflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
